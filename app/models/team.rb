cdd1c1ac6bd90efede3ac1a7adaa840aa09e0e25
steveneross94 <ross.steven@me.com>
2020-05-24 17:24:57 -0400

    still working on connections

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,4 +2,15 @@ class Team < ActiveRecord::Base
     has_many :athletes 
     has_many :brands, through: :athletes
     has_many :shoes, through: :athletes
+
+    def team_name
+        self.name
+    end
+
+    def team_city
+        self.city
+    end
+
+    
+
 end
